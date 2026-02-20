706975bb0dce4573d82af879a09088afd54778cf
holosoe <soe@holonym.id>
2026-02-20 20:44:00 +0700

    update waap-wagmi-nextjs example

--- waap-wagmi-nextjs/src/app/layout.tsx
+++ waap-wagmi-nextjs/src/app/layout.tsx
@@ -1,13 +1,9 @@
 'use client'
 
 import Header from '@/components/Header'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Slide, ToastContainer } from 'react-toastify'
-import { WagmiProvider } from 'wagmi'
 import './globals.css'
-import { config } from '@/wagmi.config'
-
-const queryClient = new QueryClient()
+import Providers from '@/components/Providers'
 
 export default function RootLayout({
   children,
@@ -18,12 +14,10 @@ export default function RootLayout({
     <html lang='en'>
       <body className={``}>
           <>
-          <WagmiProvider config={config}>
-            <QueryClientProvider client={queryClient}>
-              <Header />
-              {children}
-            </QueryClientProvider>
-          </WagmiProvider>
+          <Providers>
+            <Header />
+            {children}
+          </Providers>
           <ToastContainer transition={Slide} />
         </>
       </body>
